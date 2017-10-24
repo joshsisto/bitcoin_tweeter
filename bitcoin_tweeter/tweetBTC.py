2acca45ceee4b6410dc5a8ddda908d41218eda00
Josh Sisto <joshsisto@gmail.com>
2017-10-24 15:05:25 -0700

    Update tweetBTC.py

--- bitcoin_tweeter/tweetBTC.py
+++ bitcoin_tweeter/tweetBTC.py
@@ -4,17 +4,6 @@ from json import loads
 import urllib.request
 import tweepy
 
-# The consumer keys can be found on your application's Details
-# page located at https://dev.twitter.com/apps (under "OAuth settings")
-consumer_key=""
-consumer_secret=""
-
-# The access tokens can be found on your applications's Details
-# page located at https://dev.twitter.com/apps (located
-# under "Your access token")
-access_token=""
-access_token_secret=""
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
