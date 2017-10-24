8117030116d378e08a0febb744e77e6fa9b5bfcc
Josh Sisto <joshsisto@gmail.com>
2017-10-23 20:58:44 -0700

    Update tweetBTC.py

--- bitcoin_tweeter/tweetBTC.py
+++ bitcoin_tweeter/tweetBTC.py
@@ -1,10 +1,19 @@
 from __future__ import absolute_import, print_function
 from datetime import datetime, timedelta
 from json import loads
-from urllib import urlopen
+import urllib.request
 import tweepy
-import time
-from credentials import *
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key=""
+consumer_secret=""
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+access_token=""
+access_token_secret=""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,9 +25,10 @@ print(api.me().name)
 
 def get_bitcoin_price(endpoint):
     """query the coindesk api"""
-    req = urlopen('http://api.coindesk.com/v1/bpi/{}'.format(endpoint))
-    return loads(req.read())
-
+    req = urllib.request.Request('http://api.coindesk.com/v1/bpi/{}'.format(endpoint))
+    result = urllib.request.urlopen(req)
+    resulttext = result.read()
+    return loads(resulttext)
 
 def get_current_btc_price():
     """get bitcoin price index in US dollars by querying the coindesk API"""
@@ -27,22 +37,18 @@ def get_current_btc_price():
 
 def get_last_years_btc_price():
     """get last years bitcoin price and store is as a variable"""
-    last_year = (datetime.now() - timedelta(days=366)).strftime('%Y-%m-%d')
+    global last_year
+    last_year = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
     endpoint = 'historical/close.json?start={0}&end={0}'.format(last_year)
     return get_bitcoin_price(endpoint)['bpi']
 
 
 if __name__ == '__main__':
 
-    while True:
+    current_price = get_current_btc_price()
+    BTC_USD = ('Current BTC Price: ${rate_float:.2f} USD '.format(**current_price))
 
-        current_price = get_current_btc_price()
-        pri = ('Current BTC Price: ${rate_float:.2f} USD '.format(**current_price))
+    last_years_price = get_last_years_btc_price()
+    LY_BTC_USD = ('BTC Price on {}: ${} USD'.format(last_year, last_years_price[last_year]))
 
-        last_years_price = get_last_years_btc_price()
-        pri2 = ('BTC Price on {}: ${:.2f} USD'.format(*last_years_price.items()[0]))
-
-        api.update_status(pri + pri2 + ' #bitcoin')     # update status
-        time.sleep(86400)                               # wait 24 hours
-
-# End of Bitcoin
+    api.update_status(BTC_USD + LY_BTC_USD + ' #bitcoin')  # update status
